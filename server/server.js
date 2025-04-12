bb8f53e8188d86c08abe290a4ddcfd8e42b44a5d
Sahil Warkhade <sahilwarkhade111@gmail.com>
2025-04-13 01:29:44 +0530

    PORT fixed

--- server/server.js
+++ server/server.js
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
